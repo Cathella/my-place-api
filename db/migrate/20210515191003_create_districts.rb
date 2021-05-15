9cc1ec2c1d456044719fa0b71cd595e0f6941a28
Cathella <ncathie214@gmail.com>
2021-05-15 22:16:23 +0300

    create country, region, and district models

---
+++ db/migrate/20210515191003_create_districts.rb
@@ -0,0 +1,11 @@
+class CreateDistricts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :districts do |t|
+      t.string :name
+      t.integer :population
+      t.references :region
+
+      t.timestamps
+    end
+  end
+end
